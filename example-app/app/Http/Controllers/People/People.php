f6f39439c9567f2ee9a21b7a19d885762cb9bce5
PioBis778 <pb305347@student.polsl.pl>
2022-12-17 16:02:50 +0100

    cd

--- example-app/app/Http/Controllers/People/People.php
+++ example-app/app/Http/Controllers/People/People.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\People;
 
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
