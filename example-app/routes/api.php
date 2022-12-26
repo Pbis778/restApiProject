e4b0e238aa5f88d6a3476c69ebc290f083c8f7ea
PioBis778 <pb305347@student.polsl.pl>
2022-12-26 13:43:08 +0100

    routes

--- example-app/routes/api.php
+++ example-app/routes/api.php
@@ -3,6 +3,12 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+Route::get('pbist/305347/people', [PeopleController::class, 'index']);
+Route::get('pbist/305347/people/{people}', [PeopleController::class, 'read']);
+Route::post('pbist/305347/people', [PeopleController::class, 'create']);
+Route::put('pbist/305347/people/{people}', [PeopleController::class, 'update']);
+Route::delete('pbist/305347/people/{people}', [PeopleController::class, 'delete']);
+
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
